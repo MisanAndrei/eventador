e291af563d86dcb257862e1e8bb652122258ef7a
Misan Andrei <misan.andrei@yahoo.com>
2025-04-01 22:37:39 +0300

    changes for meta tage is supplier profile

--- src/app/components/supplier-profile-component/supplier-profile.component.ts
+++ src/app/components/supplier-profile-component/supplier-profile.component.ts
@@ -8,6 +8,7 @@ import { ApiService } from '../../Services/ApiService';
 import { AuthService } from '../../Services/AuthService';
 import { UserRole } from '../../Utilities/enums/Enums';
 import { Lightbox } from 'ng-gallery/lightbox';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-supplier-profile',
@@ -65,7 +66,7 @@ import { Lightbox } from 'ng-gallery/lightbox';
       private apiService: ApiService, 
       private gallery: Gallery,
       private lightbox: Lightbox,
-      private authService: AuthService){
+      private authService: AuthService,private meta: Meta, private titleService: Title){
       this.isMobile = this.breakpointObserver.observe(Breakpoints.Handset)
       .pipe(
         map(result => result.matches)
@@ -148,6 +149,15 @@ import { Lightbox } from 'ng-gallery/lightbox';
             this.areasOfInterest = response.areaOfInterestNames;
             this.images = response.images.map(x => new ImageItem({ src: x.imageUrl, thumb: x.imageUrl }));
             this.supplierCategories = response.categories;
+
+            this.meta.updateTag({ name: 'title', content: this.profileName || '' });
+            this.meta.updateTag({ name: 'description', content: this.motto || '' });
+
+            // Open Graph (OG) Meta Tags for Social Sharing
+            this.meta.updateTag({ property: 'og:title', content: this.profileName || '' });
+            this.meta.updateTag({ property: 'og:description', content: this.motto || '' });
+            this.meta.updateTag({ property: 'og:image', content: this.profileImage || '' });
+            this.meta.updateTag({ property: 'og:url', content: window.location.href || '' });
 
             const galleryRef = this.gallery.ref('lightboxGallery');
             galleryRef.load(this.images);
