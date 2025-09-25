893b8f8498c1bf66c721e950f0c7141c2dba7fde
Misan Andrei <misan.andrei@yahoo.com>
2025-09-25 20:21:56 +0300

    updated image slider component

--- src/app/components/image-slider-component/image-slider.component.ts
+++ src/app/components/image-slider-component/image-slider.component.ts
@@ -1,9 +1,6 @@
-import {  NgModule, Component, OnInit, ViewChild, AfterViewInit, ElementRef} from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable, map } from 'rxjs';
-
-
-
 
 @Component({
   selector: 'app-image-slider',
@@ -13,29 +10,29 @@ import { Observable, map } from 'rxjs';
 export class ImageSliderComponent implements OnInit, AfterViewInit {
   isMobile: Observable<boolean>;
   useImages: boolean = false;
-  constructor(private breakpointObserver: BreakpointObserver, private el: ElementRef) {
+
+  @ViewChildren('videoPlayer') videoPlayers!: QueryList<ElementRef<HTMLVideoElement>>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
     this.isMobile = this.breakpointObserver.observe(Breakpoints.Handset)
-      .pipe(
-        map(result => result.matches)
-      );
+      .pipe(map(result => result.matches));
   }
+
+  ngOnInit(): void {}
+
   ngAfterViewInit(): void {
-    const videoElement = this.el.nativeElement.querySelector('#myVideo');
-    this.el.nativeElement.querySelector('#myVideo').play();
-    videoElement.play();
-
-    videoElement.addEventListener('play', () => {
-      // Your code to handle the play event goes here
-    });
+    this.playActiveVideo();
+    this.videoPlayers.changes.subscribe(() => this.playActiveVideo());
   }
 
-  
-  
-
-  
-  ngOnInit(): void {
-    
+  private playActiveVideo(): void {
+    const video = this.videoPlayers.first?.nativeElement;
+    if (video) {
+      video.muted = true; // required for autoplay everywhere
+      video.setAttribute('playsinline', 'true'); // iOS Safari fix
+      video.play().catch(err => {
+        console.warn('Autoplay blocked or interrupted:', err);
+      });
+    }
   }
 }
-
-
